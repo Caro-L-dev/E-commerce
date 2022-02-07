311888bd6eafbc4774c867e5b9d00288ae6b1b80
Caro-L-dev <liardcar@gmail.com>
2022-02-07 17:06:32 +0100

    :sparkles: feat(components/Categories): Shopping categories

--- src/data.js
+++ src/data.js
@@ -23,4 +23,22 @@ export const sliderItems = [
         button: "SHOW NOW",
         bg: "f5fafd",
     },
-]
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://img-19.ccm2.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg",
+        title: "SUMMER SHIRT",
+    },
+    {
+        id: 2,
+        img: "https://img-19.ccm2.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg",
+        title: "LOUNGEWEAR",
+    },
+    {
+        id: 3,
+        img: "https://img-19.ccm2.net/WNCe54PoGxObY8PCXUxMGQ0Gwss=/480x270/smart/d8c10e7fd21a485c909a5b4c5d99e611/ccmcms-commentcamarche/20456790.jpg",
+        title: "LIGHT PANTS",
+    },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './../components/Navbar';
 import Slider from './../components/Slider';
 import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
 
 
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Announcement />
             <Navbar />
             <Slider />
+            <Categories />
         </div>
     )
 };
